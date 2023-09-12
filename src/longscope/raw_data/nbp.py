442d6a84e3e6d582f3f86e05391dee5b8d6c7d24
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2023-09-13 00:08:01 +0200

    Add logging

--- src/longscope/raw_data/nbp.py
+++ src/longscope/raw_data/nbp.py
@@ -1,10 +1,19 @@
 import datetime as dt
+import logging
 from itertools import chain
 from typing import Iterable
 
 import requests
 
 from .model import Rate
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(fmt='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 API_URL = 'https://api.nbp.pl/api/exchangerates/rates'
 DATE_RANGE_LIMIT = 367
@@ -17,6 +26,7 @@ class NBP:
     @staticmethod
     def get_data_for_code_and_date(table: str, code: str, date: str = 'today') -> Rate:
         """Returns a rate for the given code and date."""
+        logger.info(f'Fetching data from NBP for: {table=}, {code=}, {date=}')
         url = f'{API_URL}/{table}/{code}/{date}/?format=json'
         res = requests.get(url)
         res.raise_for_status()
@@ -29,6 +39,7 @@ class NBP:
         n_days = (dt.date.fromisoformat(end_date) -
                   dt.date.fromisoformat(start_date)).days
         if n_days <= DATE_RANGE_LIMIT:
+            logger.info(f'Fetching data from NBP for: {table=}, {code=}, {start_date=}, {end_date=}')
             url = f'{API_URL}/{table}/{code}/{start_date}/{end_date}/?format=json'
             res = requests.get(url)
             res.raise_for_status()
@@ -37,10 +48,8 @@ class NBP:
 
         limit_end_date = (dt.date.fromisoformat(start_date) + dt.timedelta(days=DATE_RANGE_LIMIT)).strftime(DATE_FORMAT)
         limit_start_date = (dt.date.fromisoformat(limit_end_date) + dt.timedelta(days=1)).strftime(DATE_FORMAT)
-        return chain(
-            NBP.get_data_for_code_and_date_range(table, code, start_date, limit_end_date),
-            NBP.get_data_for_code_and_date_range(table, code, limit_start_date, end_date)
-        )
+        return chain(NBP.get_data_for_code_and_date_range(table, code, start_date, limit_end_date),
+                     NBP.get_data_for_code_and_date_range(table, code, limit_start_date, end_date))
 
     @staticmethod
     def _response_to_rate(response: dict) -> Rate:
